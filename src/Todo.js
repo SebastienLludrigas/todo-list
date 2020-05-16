b446c064d09754e953304e0ceadc7432f06adfdf
Sebastien Lludrigas <sebastienlludrigas@gmail.com>
2020-05-16 19:20:32 +0200

    Section 5 fini

--- src/Todo.js
+++ src/Todo.js
@@ -19,33 +19,59 @@ export default class Todo extends Component {
          element: '',
          items: [...this.state.items, {element: this.state.element}]
       })
+   }
 
-      console.log(this.state);
+   deleteItem = (index) => {
+      const arr = this.state.items;
+      arr.splice(index, 1);
+      this.setState({
+         items: arr
+      })
+   }
+
+   renderTodo = () => {
+      return this.state.items.map((item, index) => {
+         return (
+            <div className="card mb-3" key={index}>
+               <div className="card-body">
+                  <h4>{item.element}
+                  <i className="fas fa-times"
+                  style={{cursor: 'pointer', float: 'right', color: 'red'}}
+                  onClick={() => this.deleteItem(index)}
+                  ></i>
+                  </h4>
+               </div>
+            </div>
+         )
+      })
    }
 
    render() {
       return (
-         <div className="card my-3">
-            <div className="card-header">To-Do List</div>
-            <div className="card-body">
+         <React.Fragment>
+            <div className="card my-3">
+               <div className="card-header">To-Do List</div>
+               <div className="card-body">
 
-               <form onSubmit={this.onSubmit}>
-                  <div className="form-group">
+                  <form onSubmit={this.onSubmit}>
+                     <div className="form-group">
 
-                     <label htmlFor="element">Chose à faire</label>
-                     <input
-                     className="form-control form-control-lg"
-                     name="element"
-                     onChange={this.onChange}
-                     value={this.state.element}
-                     />
+                        <label htmlFor="element">Chose à faire</label>
+                        <input
+                        className="form-control form-control-lg"
+                        name="element"
+                        onChange={this.onChange}
+                        value={this.state.element}
+                        />
 
-                  </div>
-                  <button className="btn btn-primary btn-block">Ajouter une chose à faire !</button>
-               </form>
+                     </div>
+                     <button className="btn btn-primary btn-block">Ajouter une chose à faire !</button>
+                  </form>
 
-            </div>      
-         </div>
+               </div>      
+            </div>
+            {this.renderTodo()}
+         </React.Fragment>   
       )
    }
 }
