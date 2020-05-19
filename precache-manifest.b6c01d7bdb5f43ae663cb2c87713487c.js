4422bac91d8c2a617a815cf923fb1c87fb7ee5e9
Sebastien Lludrigas <sebastienlludrigas@gmail.com>
2020-05-19 06:54:19 +0200

    Updates

---
+++ precache-manifest.b6c01d7bdb5f43ae663cb2c87713487c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed09f8096b156deab3256542bd7cdfb",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "963ba16d7cfb8a269491",
+    "url": "/todo-list/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "963ba16d7cfb8a269491",
+    "url": "/todo-list/static/js/2.b06c2574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.b06c2574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "138ebecb07b534b52a62",
+    "url": "/todo-list/static/js/main.582b7ab1.chunk.js"
+  },
+  {
+    "revision": "0e635de1e5e33cbe5d76",
+    "url": "/todo-list/static/js/runtime-main.072a9b53.js"
+  }
+]);
